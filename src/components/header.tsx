e510045517515e04eb332f3d931ce6f09532ec8e
Vitor Gabriel <vitorgabsg@gmail.com>
2025-03-18 16:55:12 -0300

    feat: hide/show elements depending on the current screen

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,16 @@
 import { MapPin, ShoppingCart } from '@phosphor-icons/react'
-import { Link } from 'react-router'
+import { Link, useLocation } from 'react-router'
 
 import { useCart } from '../hooks/use-cart'
 
 import logo from '../assets/logo.svg'
 
 export function Header() {
-  const { cartItemCount } = useCart()
+  const { pathname } = useLocation()
+  const { cartItemCount, orders } = useCart()
+
+  const onSuccessPage = pathname.includes('/success')
+  const order = orders[orders.length - 1]
 
   return (
     <header className="mx-auto flex max-w-6xl justify-between py-8">
@@ -15,17 +19,19 @@ export function Header() {
       </Link>
 
       <div className="flex gap-3">
-        <span className="bg-purple-light text-purple-dark flex items-center gap-1 rounded-md p-2 text-sm">
-          <MapPin weight="fill" className="text-purple size-5.5" />
-          Recife, PE
-        </span>
+        {onSuccessPage && (
+          <span className="bg-purple-light text-purple-dark flex items-center gap-1 rounded-md p-2 text-sm capitalize">
+            <MapPin weight="fill" className="text-purple size-5.5" />
+            {`${order.city}, ${order.state}`}
+          </span>
+        )}
 
         <Link
           to="/checkout"
           className="bg-yellow-light relative rounded-md p-2"
         >
           <ShoppingCart weight="fill" className="text-yellow-dark size-5.5" />
-          {cartItemCount > 0 && (
+          {!onSuccessPage && (
             <div className="bg-yellow-dark absolute top-0 right-0 flex size-5 translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-full">
               <span className="text-xs font-bold text-white">
                 {cartItemCount}
